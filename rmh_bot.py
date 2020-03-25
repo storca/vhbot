db50751f7b7e615c529c8eb18a46e5d8b58b7e2b
storca <storca@mail.com>
2020-03-25 15:01:11 +0100

    Bug fixes

--- rmh_bot.py
+++ rmh_bot.py
@@ -10,6 +10,8 @@ global raise_your_hand_message
 raised_hand_users = list()
 raise_your_hand_message = None
 
+#TODO : delete request messages
+
 @bot.event
 async def on_ready():
     print("Logged in as %s %s", bot.user.name, bot.user.id)
@@ -19,11 +21,11 @@ async def q(ctx):
     """
     When the person asks for a raise your hand event
     """
+    global raise_your_hand_message
     #TODO access control if ctx.message.author == 
     if raise_your_hand_message == None:
-        msg = await ctx.send(c.raise_your_hand_text)
-        raise_your_hand_message = msg
-        await msg.add_reaction(c.raised_hand_emoji)
+        raise_your_hand_message = await ctx.send(c.raise_your_hand_text)
+        await raise_your_hand_message.add_reaction(c.raised_hand_emoji)
     else:
         await ctx.send(c.multiple_rmh_messages_error)
 
@@ -32,39 +34,54 @@ async def on_reaction_add(reaction, user):
     """
     When a reaction is added to a message
     """
-    if reaction.message.author == bot.user:
-        pass
-    elif reaction.emoji == c.raised_hand_emoji:
-        #Then the user has raised his hand
-        raised_hand_users.append(user)
-        #Rename the user
-        await user.change_nickname(c.raised_hand_prefix + user.display_name)
+    global raised_hand_users
+    global raise_your_hand_message
+    if reaction.message.id == raise_your_hand_message.id:
+        print("reaction added")
+        #async for user in reaction.users():
+        if user == bot.user:
+            print("not renaming")
+        elif reaction.emoji == c.raised_hand_emoji:
+            #Then the user has raised his hand
+            if user not in raised_hand_users:
+                raised_hand_users.append(user)
+                #Rename the user
+                print("renaming")
+                await user.edit(nick=c.raised_hand_prefix + user.display_name)
 
 @bot.event
 async def on_reaction_remove(reaction, user):
     """
     """
-    if reaction.message.author == bot.user:
-        pass
-    elif reaction.emoji == c.raised_hand_emoji:
-        #Rename the user accordingly
-        for k, r_user in enumerate(raised_hand_users):
-            if r_user == user:
-                user.change_nickname(user.display_name[len(c.raised_hand_prefix):]) #remove the prefix
-                raised_hand_users.pop(k) #dirty : remove him from the list
-                return 
-        print("User not found in rasied_hand_users")
+    global raised_hand_users
+    global raise_your_hand_message
+    if reaction.message.id == raise_your_hand_message.id:
+        print(user)
+        if user == bot.user:
+            pass
+        elif reaction.emoji == c.raised_hand_emoji:
+            #Rename the user accordingly
+            for k, r_user in enumerate(raised_hand_users):
+                if r_user == user:
+                    await user.edit(nick=user.display_name[len(c.raised_hand_prefix):]) #remove the prefix
+                    raised_hand_users.pop(k) #dirty : remove him from the list
+                    return 
+            print("User not found in rasied_hand_users")
 
 @bot.command()
 async def e(ctx):
     """
     End the raise my hand
     """
+    global raised_hand_users
+    global raise_your_hand_message
     if raise_your_hand_message != None:
         await raise_your_hand_message.delete()
         for user in raised_hand_users:
-            user.change_nickname(user.display_name[len(c.raised_hand_prefix):])
+            if not user == bot.user:
+                await user.edit(nick=user.display_name[len(c.raised_hand_prefix):])
         raised_hand_users = list()
+        raise_your_hand_message = None
 
 
 bot.run(c.token)
--- rmh_constants.py
+++ rmh_constants.py
@@ -3,7 +3,8 @@ A file where all constants are defined
 """
 token = "mysuperstylishtoken"
 cmd_prefix = '$'
-raise_your_hand_text = "Raise you hand !"
-rasied_hand_emoji = ":hand_splayed:"
+raise_your_hand_text = "Raise your hand !"
+raised_hand_emoji = b'\xe2\x9c\x8b'
 raised_hand_prefix = "."
 multiple_rmh_messages_error = "You can only send one raise my end message, to end the previous, type " + cmd_prefix + "e"
+raised_hand_emoji = raised_hand_emoji.decode("utf8")
