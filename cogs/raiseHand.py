96830fcdc55227be9eb8148ba2610efb53ee8302
thebigblase <thebigblase@gmail.com>
2020-03-30 20:52:46 +0200

    initial commit, lets do the second one instead

---
+++ cogs/raiseHand.py
@@ -0,0 +1,131 @@
+import discord 
+from discord.ext import commands 
+from colorama import Fore 
+import rmh_constants as c
+
+class RaiseHand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot 
+        self.raised_hand_users = list() 
+        self.raise_your_hand_message = None 
+        self.nicknames =[] 
+        self.channel = None 
+   
+
+
+
+    @commands.command()
+    async def q(self, ctx):
+        """
+        Asks people to raise their hands
+        """
+        print("q")
+        #TODO access control if ctx.message.author ==
+        if self.raise_your_hand_message == None:
+            self.raise_your_hand_message = await ctx.send(c.raise_your_hand_text)
+            await self.raise_your_hand_message.add_reaction(c.raised_hand_emoji)
+            await ctx.message.delete()
+        else:
+            await ctx.send(c.multiple_rmh_messages_error)
+   
+
+
+    @commands.command()
+    async def join(self, ctx):
+        """
+        Connects to the same channel the user is in
+        """
+        print("join")
+        if self.channel == None:
+            print(self.channel)
+            if type(ctx.message.author.voice) == discord.VoiceState and type(ctx.message.author.voice.channel) == discord.VoiceChannel:
+                self.channel = ctx.message.author.voice.channel
+                await self.channel.connect()
+            else:
+                await ctx.send("You're not connected to a voice channel")
+
+
+
+    @commands.command()
+    async def leave(self, ctx):
+        """
+        Leaves the voice channel
+        """
+        print("leave")
+        for client in self.bot.voice_clients:
+            await client.disconnect()
+        self.channel = None
+
+   
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        """
+        When a reaction is added to a message
+        """
+        if self.raise_your_hand_message != None and reaction.message.id == self.raise_your_hand_message.id:
+            print("reaction added")
+            #async for user in reaction.users():
+            if user == self.bot.user:
+                print("bot reacted")
+            elif reaction.emoji == c.raised_hand_emoji:
+                #Then the user has raised his hand
+                if user not in self.raised_hand_users:
+                    self.nicknames.append(user.display_name)
+                    self.raised_hand_users.append(user)
+                    #Rename the user
+                    print("renaming")
+                    await user.edit(nick=c.raised_hand_prefix + user.display_name)
+                    for client in bot.voice_clients:
+                        s = discord.FFmpegPCMAudio(c.sound_path, executable='ffmpeg')
+                        if not client.is_playing():
+                            client.play(s)
+
+   
+
+    @commands.Cog.listener()
+    async def on_reaction_remove(self, reaction, user):
+        """
+        When a reation is removed from the raise your hand message
+        """
+        print("reaction remove") 
+        if self.raise_your_hand_message != None and reaction.message.id == self.raise_your_hand_message.id:
+            print(user)
+            if user == bot.user:
+                pass
+            elif reaction.emoji == c.raised_hand_emoji:
+                #Rename the user accordingly
+                for k, r_user in enumerate(raised_hand_users):
+                    if r_user == user:
+                        await user.edit(nick=self.nicknames[k])
+                        self.raised_hand_users.pop(k) #dirty : remove him from the list
+                        self.nicknames.pop(k)
+                        return
+                print("User not found in rasied_hand_users")
+
+   
+
+    @commands.command()
+    async def e(self, ctx):
+        """
+        End the struggle
+        """
+        print("E")
+        if self.raise_your_hand_message != None:
+            await ctx.message.delete()
+            await self.raise_your_hand_message.delete()
+            self.raise_your_hand_message = None
+            for k, user in enumerate(self.raised_hand_users):
+                if not user == self.bot.user:
+                    await user.edit(nick=self.nicknames[k])
+                    print(user.display_name)
+                    self.raised_hand_users.pop(k)
+            self.raised_hand_users = list()
+            self.nicknames = list()
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(RaiseHand(bot))
--- rmh_bot.py
+++ rmh_bot.py
@@ -1,126 +1,20 @@
 import discord
 from discord.ext import commands
 import rmh_constants as c
+from colorama import Fore 
 
 bot = commands.Bot(command_prefix=c.cmd_prefix)
 
-global raised_hand_users
-global raise_your_hand_message
-global nicknames
-global channel
+extensions = ['cogs.raiseHand']
 
-raised_hand_users = list()
-nicknames = list()
-raise_your_hand_message = None
-channel = None
+if __name__ == '__main__':
+    for ext in extensions:
+        print(ext)
+        bot.load_extension(ext)
 
 @bot.event
 async def on_ready():
-    print("Logged in as %s %s" % (bot.user.name, bot.user.id))
+    print("Logged in as %s" % (bot.user.name))
 
-@bot.command()
-async def q(ctx):
-    """
-    Asks people to raise their hands
-    """
-    global raise_your_hand_message
-    #TODO access control if ctx.message.author == 
-    if raise_your_hand_message == None:
-        raise_your_hand_message = await ctx.send(c.raise_your_hand_text)
-        await raise_your_hand_message.add_reaction(c.raised_hand_emoji)
-        await ctx.message.delete()
-    else:
-        await ctx.send(c.multiple_rmh_messages_error)
 
-@bot.command()
-async def join(ctx):
-    """
-    Connects to the same channel the user is in
-    """
-    global channel
-    if channel == None:
-        print(channel)
-        if type(ctx.message.author.voice) == discord.VoiceState and type(ctx.message.author.voice.channel) == discord.VoiceChannel:
-            channel = ctx.message.author.voice.channel
-            await channel.connect()
-        else:
-            await ctx.send("You're not connected to a voice channel")
-    
-@bot.command()
-async def leave(ctx):
-    """
-    Leaves the voice channel
-    """
-    for client in bot.voice_clients:
-        await client.disconnect()
-    channel = None
-
-@bot.event
-async def on_reaction_add(reaction, user):
-    """
-    When a reaction is added to a message
-    """
-    global raised_hand_users
-    global raise_your_hand_message
-    global nicknames
-
-    if raise_your_hand_message != None and reaction.message.id == raise_your_hand_message.id:
-        print("reaction added")
-        #async for user in reaction.users():
-        if user == bot.user:
-            print("not renaming")
-        elif reaction.emoji == c.raised_hand_emoji:
-            #Then the user has raised his hand
-            if user not in raised_hand_users:
-                nicknames.append(user.display_name)
-                raised_hand_users.append(user)
-                #Rename the user
-                print("renaming")
-                await user.edit(nick=c.raised_hand_prefix + user.display_name)
-                for client in bot.voice_clients:
-                    s = discord.FFmpegPCMAudio(c.sound_path, executable='ffmpeg')
-                    if not client.is_playing():
-                        client.play(s)
-
-@bot.event
-async def on_reaction_remove(reaction, user):
-    """
-    When a reation is removed from the raise your hand message
-    """
-    global raised_hand_users
-    global raise_your_hand_message
-    global nicknames
-    if raise_your_hand_message != None and reaction.message.id == raise_your_hand_message.id:
-        print(user)
-        if user == bot.user:
-            pass
-        elif reaction.emoji == c.raised_hand_emoji:
-            #Rename the user accordingly
-            for k, r_user in enumerate(raised_hand_users):
-                if r_user == user:
-                    await user.edit(nick=nicknames[k])
-                    raised_hand_users.pop(k) #dirty : remove him from the list
-                    nicknames.pop(k)
-                    return 
-            print("User not found in rasied_hand_users")
-
-@bot.command()
-async def e(ctx):
-    """
-    End the struggle
-    """
-    global raised_hand_users
-    global raise_your_hand_message
-    global nicknames
-    if raise_your_hand_message != None:
-        await ctx.message.delete()
-        await raise_your_hand_message.delete()
-        raise_your_hand_message = None
-        for k, user in enumerate(raised_hand_users):
-            if not user == bot.user:
-                await user.edit(nick=nicknames[k])
-                print(user.display_name)
-                raised_hand_users.pop(k)
-        raised_hand_users = list()
-        nicknames = list()
-bot.run(c.token)
+bot.run("NDY2NTI4MzI4MDYxNDE5NTIx.XoI51g.rPdx22z764NJSANbxVhgtFhM8xo")
